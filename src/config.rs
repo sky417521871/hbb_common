64138ba3291c6a3f60c106d3e200d9a8b988770f
sky417521871 <417521871@qq.com>
2025-10-20 04:51:32 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -109,7 +109,6 @@ pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
     _ => PUBLIC_RS_PUB_KEY,
 };
-
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
 pub const WS_RENDEZVOUS_PORT: i32 = 21118;
